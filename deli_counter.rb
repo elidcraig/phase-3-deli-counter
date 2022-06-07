f503c7a81ec397cb7816dfacc65b382bb710ffcd
Eli Craig <elidcraig@gmail.com>
2022-06-07 10:23:27 -0700

    commit tree f32e2bcb59b42cc891c658d54e87234991207998

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def line deli_array
+  if deli_array.length == 0  
+    puts 'The line is currently empty.' 
+  else 
+    count = 0
+    while count < deli_array.length
+      deli_array[count] = "#{count + 1}. #{deli_array[count]}"
+      count += 1
+    end  
+    puts "The line is currently: #{deli_array.join(' ')}"
+  end
+end
+
+def take_a_number deli_array, new_name
+  deli_array.push(new_name)
+  puts "Welcome, #{new_name}. You are number #{deli_array.length} in line."
+end  
+
+def now_serving deli_array
+  if deli_array.length == 0
+    puts 'There is nobody waiting to be served!'
+  else 
+    puts "Currently serving #{deli_array.first}."
+    deli_array.shift
+  end    
+end  
+# binding.pry
